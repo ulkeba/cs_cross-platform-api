6ccf811f86c33989713efbce27f208bc2a8d3762
Bastian Ulke <bastian.ulke@microsoft.com>
2022-01-14 15:41:22 +0100

    Initial commit.

---
+++ ContainerObject.cs
@@ -0,0 +1,8 @@
+namespace SimpleApi;
+
+public class ContainerObject
+{
+
+    public int NumbersGenerated { get; set; }
+    public Double TimeUsed { get; set; }
+}
---
+++ build-containers.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+docker build --pull -t randommultiplications:linux -f Dockerfile-linux .
